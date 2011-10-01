7784877ff714d05beeaf2c7b71f4e76affe6fb65
Masaki Saito <rezoolab@gmail.com>
2011-10-02 00:38:24 +0900

    divide generate_test.cpp

--- testings/generate_test.cpp
+++ testings/generate_test.cpp
@@ -19,6 +19,14 @@ TEST(algorithmTest, generate_image)
     }
     cvutil::generate_image(src2, [](){ return 5; });
     ASSERT_TRUE(std::equal(src1.begin(), src1.end(), src2.begin()));
+}
+
+TEST(algorithmTest, generate_image_xy)
+{
+    const int width = 100;
+    const int height = 100;
+    cv::Mat_<int> src1(height, width);
+    cv::Mat_<int> src2(height, width);
 
     for(int y=0; y<src1.rows; ++y) {
         for(int x=0; x<src1.cols; ++x) {
@@ -27,7 +35,15 @@ TEST(algorithmTest, generate_image)
     }
     cvutil::generate_image_xy(src2, [](int x, int y){ return x + y; });
     ASSERT_TRUE(std::equal(src1.begin(), src1.end(), src2.begin()));
- 
+}
+
+TEST(algorithmTest, generate_image_uv)
+{
+    const int width = 100;
+    const int height = 100;
+    cv::Mat_<int> src1(height, width);
+    cv::Mat_<int> src2(height, width);
+
     for(int y=0; y<src1.rows; ++y) {
         for(int x=0; x<src1.cols; ++x) {
             int u = x - width/2;
@@ -35,6 +51,6 @@ TEST(algorithmTest, generate_image)
             src1(y, x) = u*v;
         }
     }
-    cvutil::generate_image_uv(src2, [](int u, int v){ return u*v; });
+    cvutil::generate_image_uv(src2, [](int u, int v){ return u * v; });
     ASSERT_TRUE(std::equal(src1.begin(), src1.end(), src2.begin()));
 }
